9323cef7def8ee21320945eefd40883e6d72b616
hisc94 <57772867+hisc94@users.noreply.github.com>
2021-07-06 14:54:22 -0500

    fixed style

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { TextInput } from 'react-native'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Input } from 'react-native-elements'
+import { Button, Input, Icon } from 'react-native-elements'
 
 export default function RegisterForm() {
+    const [showPassword, setshowPassword] = useState(false) //se usa para mostrar o no la contraseña
+    const [showPassword2, setshowPassword2] = useState(false) //se usa para mostrar o no la contraseña
     return (
         <View style={styles.form}>
             <Input
@@ -14,13 +15,34 @@ export default function RegisterForm() {
                 containerstyle={styles.input}
                 placeholder="Ingrese su contraseña.."
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline" }
+                        iconStyle={styles.icon}
+                        onPress={()=>setshowPassword(!showPassword)}
+                    />
+                }
             />
             <Input
                 containerstyle={styles.input}
                 placeholder="Confrima la contraseña.."
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword2}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={showPassword2 ? "eye-off-outline" : "eye-outline" } //if de una linea
+                        iconStyle={styles.icon}
+                        onPress={()=>setshowPassword2(!showPassword2)}
+                    />
+                }
+            />
+            <Button
+                title="Registrar Nuevo Usuario"
+                containerStyle={styles.btnContainer}
+                buttonStyle={styles.btn}
             />
         </View>
     )
@@ -32,5 +54,16 @@ const styles = StyleSheet.create({
     },
     input:{
         width:"100%"
+    },
+    btnContainer:{
+        marginTop:20,
+        width:"95%",
+        alignSelf:"center"
+    },
+    btn:{
+        backgroundColor:"#442484"
+    },
+    icon:{
+        color:"#c1c1c1"
     }
 })
--- screens/account/Register.js
+++ screens/account/Register.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import RegisterForm from '../../components/account/RegisterForm'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view' //para poder moverel formulario cuando aparece el teclado cm
 
 export default function Register() {
     return (
-        <View>
+        <KeyboardAwareScrollView>
+            <Image
+                source={require("../../assets/restaurant-logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+            />
             <RegisterForm/>
-        </View>
+        </KeyboardAwareScrollView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:30
+    },
+    image:{
+        height:150,
+        width:"100%",
+        marginBottom:20
+    },
+})
